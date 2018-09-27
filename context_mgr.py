97196dadc60e9342c44f2c52a27abf9f73ef887c
Youngseog Chung <youngsec@andrew.cmu.edu>
2018-09-27 13:07:04 -0400

    implemented contextual_mgr class

---
+++ context_mgr.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+def ConstructKey(input_list):
+    """
+    Takes an input list of numpy arrays
+    Reshape each array into 1D 
+    Append each array into a 1D list
+    Construct a tuple with the 1D list
+    """
+    out_list = []
+    for i in input_list:
+        out_list = out_list + (i.flatten()).tolist()
+    
+    return tuple(out_list)
+
+class ContextManager():
+    def __init__(self):
+        self.opt_data = {}
+        
+    def add(self, ctx, (opt_val, opt_pt)):
+        self.opt_data.update({ConstructKey(ctx): (opt_val, opt_pt)})
+
