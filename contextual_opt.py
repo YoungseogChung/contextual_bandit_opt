b6d0862edf34e207c96c14b2c311dbbf3df9ed88
IanChar <ianchar4@gmail.com>
2018-09-28 17:42:09 -0400

    Added print method, fixed random alpha dims

--- contextual_opt.py
+++ contextual_opt.py
@@ -10,16 +10,19 @@ from synthetic_contextual_fcn import hartmann6
 
 from context_mgr import ContextManager
 
-sys.path.append('/home/master/programs/dragonfly')
+sys.path.append('/home/ian/Documents/projects/dragonfly')
+# sys.path.append('/home/master/programs/dragonfly')
 from dragonfly import maximise_function
 
 def optimize_fixed_context(fixed_func, max_capital, domain_bds):
     # TODO: I think that this flow should be improved if/when class for
     # managing a function's context is created.
-    opt_val, opt_pt, _ = maximise_function(fixed_func, max_capital, domain_bounds=domain_bds)
+    opt_val, opt_pt, _ = maximise_function(fixed_func, max_capital,
+                                           domain_bounds=domain_bds)
     return (opt_val, opt_pt)
 
-def optimize_over_contexts(func_wrapper, ctxs, max_capital, domain_bds, ctx_manager):
+def optimize_over_contexts(func_wrapper, ctxs, max_capital, domain_bds,
+                           ctx_manager):
     """
     Finds optimum for each of the given contexts.
     func_wrapper: Wrapped function to be optimized.
@@ -28,16 +31,20 @@ def optimize_over_contexts(func_wrapper, ctxs, max_capital, domain_bds, ctx_mana
         """
     for ctx in ctxs:
         fixed_func = func_wrapper.get_contexed_func(ctx)
-        opt_val, opt_pt = optimize_fixed_context(fixed_func, max_capital, domain_bds)
+        opt_val, opt_pt = optimize_fixed_context(fixed_func, max_capital,
+                                                 domain_bds)
         ctx_manager.add(ctx, (opt_val, opt_pt))
-        print opt_val, opt_pt
     return ctx_manager
 
 if __name__ == '__main__':
-    contexts = [[np.random.rand(4, 1),
+    contexts = [[np.random.rand(4),
                  np.random.rand(4, 6),
                  np.random.rand(4, 6)] for _ in xrange(3)]
-    ctx_manager = ContextManager() 
+    ctx_manager = ContextManager()
     domains = [[0, 1] for _ in xrange(6)]
     full_func = FunctionContextWrapper(hartmann6)
+    tmp = full_func.get_contexed_func(contexts[0])
+    print tmp(np.random.rand(1, 6))
+    print hartmann6(np.random.rand(1, 6), contexts[0][0], contexts[0][1], contexts[0][2])
     optimize_over_contexts(full_func, contexts, 5, domains, ctx_manager)
+    ctx_manager.print_stored()
--- synthetic_contextual_fcn.py
+++ synthetic_contextual_fcn.py
@@ -1,5 +1,16 @@
 import numpy as np
 
+# The values of the hartmann6 function as given by
+# https://www.sfu.ca/~ssurjano/hart6.html
+STANDARD_ALPHA = np.array([1.0, 1.2, 3.0, 3.2])
+STANDARD_A = np.array([[10, 3, 17, 3.50, 1.7, 8],\
+                       [0.05, 10, 17, 0.1, 8, 14],\
+                       [3, 3.5, 1.7, 10, 17, 8],\
+                       [17, 8, 0.05, 10, 0.1, 14]])
+STANDARD_P = 10e-5 * np.array([[1312, 1696, 5569, 124, 8283, 5886],\
+                               [2329, 4135, 8307, 3736, 1004, 9991],\
+                               [2348, 1451, 3522, 2883, 3047, 6650],\
+                               [4047, 8828, 8732, 5743, 1091, 381]])
 
 def hartmann6(X, alpha, A, P):
     """
@@ -8,31 +19,17 @@ def hartmann6(X, alpha, A, P):
     A: 4x6
     P: 4x6
     """
-    out =(-1)*np.sum(alpha*np.exp(np.sum((-1)*A*(X-P)**2, axis=1)),axis=0)
+    return (-1)*np.sum(alpha*np.exp(np.sum((-1)*A*(X-P)**2, axis=1)),axis=0)
 
-    return out
-
-def main(X, alpha, A, P):
+def main(X, alpha=None, A=None, P=None):
     """
     minimum achieved for default alpha, A, P when
     X = np.array([0.20169, 0.150011, 0.476874,0.275332, 0.311652, 0.6573])
     """
-    
-    if alpha is None:
-        alpha = np.array([1.0,1.2,3.0,3.2])
-
-    if A is None:
-        A = np.array([[10,   3,   17,  3.50, 1.7, 8],\
-                    [  0.05, 10,  17,  0.1,  8,   14], \
-                    [  3,    3.5, 1.7, 10,   17,  8],\
-                    [  17,   8,   0.05,10,   0.1, 14]])
-
-    if P is None:
-        P = 10e-5*np.array([[1312, 1696, 5569, 124,  8283, 5886],\
-                           [ 2329, 4135, 8307, 3736, 1004, 9991], \
-                           [ 2348, 1451, 3522, 2883, 3047, 6650],\
-                           [ 4047, 8828, 8732, 5743, 1091, 381]])
-
-
-    print(hartmann6(X, alpha, A, P))
+    alpha = STANDARD_ALPHA if alpha is None else alpha
+    A = STANDARD_A if A is None else A
+    P = STANDARD_P if P is None else P
     return hartmann6(X, alpha, A, P)
+
+if __name__ == '__main__':
+    print main(np.random.rand(1, 6))
